a669baed73abc11b63885a826c010b78cb4b00ee
garsipal <48388897+garsipal@users.noreply.github.com>
2022-10-25 07:21:11 +0200

    update comment and indentation on `main.py`

--- bot/main.py
+++ bot/main.py
@@ -26,8 +26,7 @@ async def on_ready():
     await client.change_presence(activity = discord.Game(name = "Valhalla"))
     print("Valkyria is ready")
 
-# The channel ID to which the bot will send embedded messages.
-# To access the channel and server, the bot will obviously need the necessary permissions. 
+# The channel ID where the bot will, providing it has the necessary permissions, send embedded messages. 
 channel_id = 948332733267054623
 
 # Send an embed with particular colors, random messages, and the user's avatar as a thumbnail when a user joins the server.
@@ -36,13 +35,17 @@ async def on_member_join(member):
     print('Someone joined the server')
     embed = discord.Embed()
     embed.title = ":green_circle: Join"
-    welcome_message_list = [ 'has joined the party', 'has been chosen', 'is here', 'has joined us', 'has entered Valhalla', 'has joined the server' ]          
-    welcome_message = welcome_message_list[random.randint(0, len(welcome_message_list) - 1 )]
-    welcome_comment_list = [ ':wave:', 'Greetings and salutations.', 'Hello!', 'Howdy, partner.',
-                             'Ciao!', 'Hola!', 'Welcome to the club.', 'How is life sailing?', 'Hallo!', 'Enjoy your stay.', 
-                             "It's good to see you.", "Cool.", 'We hope you brought a cake.', "I've been waiting for ages for someone to come along.",
-                             'Welcome!', 'Hey there.', 'So, we meet at last.', 'Salut!', '' ]
-    welcome_comment = welcome_comment_list[random.randint(0, len(welcome_comment_list) -1 )]
+    welcome_message_list = ['has joined the party', 'has been chosen', 'is here',
+                            'has joined us', 'has entered Valhalla', 'has joined the server']          
+    welcome_message = welcome_message_list[random.randint(0, len(welcome_message_list) - 1)]
+    welcome_comment_list = [':wave:', 'Greetings and salutations.', 'Welcome to the club.',
+                            'Welcome!', 'We hope you brought a cake.', "I've been waiting for ages for someone to come along.",
+                            'Ciao!', 'Hola!', 'Hello!',
+                            'Hallo!', 'Salut!', 'Hey there.',
+                            'Howdy, partner.', "It's good to see you.", 'How is life sailing?',
+                            'Enjoy your stay.', 'So, we meet at last.', "Cool.",
+                            '']
+    welcome_comment = welcome_comment_list[random.randint(0, len(welcome_comment_list) - 1)]
     embed.description = "{} **{}**. ".format(member.mention, welcome_message) + welcome_comment
     embed.set_thumbnail(url = member.avatar_url)
     embed.color = 0x78b159
@@ -55,12 +58,15 @@ async def on_member_remove(member):
     print('Someone left the server')
     embed = discord.Embed()
     embed.title = ":red_circle: Leave"
-    unwelcome_comment_list = [ ':wave:', 'Take care, butterfly.', "Don't forget to come back!", "Don't say goodbye!", 'Fare thee well.',
-                               "In case I don't see you - good afternoon, good evening, and good night!",'Bye.', 'What a pity.',
-                               'Catch you on the rebound.', 'Adieu!', 'Auf Wiedersehen!', 'Adios.', 'Ciao ciao.', 'Au revoir!', 'Did I say something wrong?',
-                               "Goodbye and don't cry, we won't.", 'It has been emotional.', 'Oh well.', 'It was fun while it lasted.', 
-                               'That means more cake for us.', "We will probably never forget you... probably.", '' ]
-    unwelcome_comment = unwelcome_comment_list[random.randint(0, len(unwelcome_comment_list) - 1 )]
+    unwelcome_comment_list = [':wave:', 'Take care, butterfly.', "Don't forget to come back!",
+                              "Don't say goodbye!", 'Fare thee well.', "In case I don't see you - good afternoon, good evening, and good night!",
+                              'Bye.', 'What a pity.', 'Catch you on the rebound.',
+                              'Adieu!', 'Auf Wiedersehen!', 'Adios.',
+                              'Ciao ciao.', 'Au revoir!', "Goodbye and don't cry, we won't.",
+                              'It has been emotional.', 'Oh well.', 'It was fun while it lasted.', 
+                              'Did I say something wrong?', 'That means more cake for us.', "We will probably never forget you... probably.",
+                              '']
+    unwelcome_comment = unwelcome_comment_list[random.randint(0, len(unwelcome_comment_list) - 1)]
     embed.description = "{} **has left the server**. ".format(member.mention) + unwelcome_comment
     embed.set_thumbnail(url = member.avatar_url)
     embed.color = 0xff473e
